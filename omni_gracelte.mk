ed68aeabd3b6197a2fd364168dbd1efa5992dbff
HostZero <haneesh.da.rocker@gmail.com>
2016-08-05 21:16:55 +0530

    gracelte: few graceltexx patches

--- omni_gracelte.mk
+++ omni_gracelte.mk
@@ -16,11 +16,15 @@ PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=msamsung/graceltexx/gracelte:6.0.1/MMB29K/N930FXXU1APG7:user/release-keys \
+    BUILD_FINGERPRINT=samsung/omni_graceltexx/gracelte:6.0.1/MMB29K/N930FXXU1APG7:user/release-keys \
     PRIVATE_BUILD_DESC="graceltexx-user 6.0.1 MMB29K N930FXXU1APG7 release-keys"
 
 # device info more...
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.model=SM-N930F \
     ro.product.name=graceltexx \
-    ro.product.device=gracelte 
+    ro.product.device=gracelte \ 
+    ro.omni.device=graceltexx \
+    ro.build.flavor=omni_graceltexx-eng \
+    ro.modversion=OmniROM-6.0.1-$(shell date -u +%Y%m%d)-graceltexx-HOMEMADE \
+    ro.omni.version=6.0.1-$(shell date -u +%Y%m%d)-graceltexx-HOMEMADE \
